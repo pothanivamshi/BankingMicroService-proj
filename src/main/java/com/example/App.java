91d6db7b33ebb1496f9d262a64f2d76a57f13b46
pothanivamshi <140006067+pothanivamshi@users.noreply.github.com>
2024-11-19 10:14:56 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -15,6 +15,6 @@ public class App {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello pothani vamshi, Banking Microservice!";
+        return "Hello pothani vamshi. Welcome, Banking Microservice!";
     }
 }
