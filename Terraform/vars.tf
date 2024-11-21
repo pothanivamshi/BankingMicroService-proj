fa2b3042aba32867225784933c75084818db8636
pothanivamshi <140006067+pothanivamshi@users.noreply.github.com>
2024-11-21 11:30:25 +0530

    Update vars.tf

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -1,117 +1,74 @@
-# vars.tf
-
-# Subscription ID for the Azure provider
 variable "subscription_id" {
-  description = "The Azure subscription ID to use"
+  description = "Azure Subscription ID"
   type        = string
 }
 
-# Resource Group Name
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
-# Location for resources (e.g., "Canada Central")
 variable "location" {
-  description = "The Azure region where resources will be created"
+  description = "Location for the resources"
   type        = string
 }
 
-# Virtual Network Name
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
 variable "vnet_name" {
-  description = "The name of the virtual network"
+  description = "Name of the Virtual Network"
   type        = string
 }
 
-# Address Space for Virtual Network
-variable "vnet_address_space" {
-  description = "The address space for the virtual network"
+variable "address_space" {
+  description = "Address space for the Virtual Network"
   type        = list(string)
 }
 
-# Subnet Name
 variable "subnet_name" {
-  description = "The name of the subnet"
+  description = "Name of the Subnet"
   type        = string
 }
 
-# Subnet Address Prefixes
-variable "subnet_address_prefixes" {
-  description = "The address prefixes for the subnet"
+variable "subnet_address_prefix" {
+  description = "Address prefix for the Subnet"
   type        = list(string)
 }
 
-# Network Security Group Name
 variable "nsg_name" {
-  description = "The name of the Network Security Group"
+  description = "Name of the Network Security Group"
   type        = string
 }
 
-# Public IP Name
-variable "public_ip_name" {
-  description = "The name of the public IP address"
+variable "vm_admin_username" {
+  description = "Admin username for the VM"
   type        = string
 }
 
-# Virtual Machine Network Interface Name
-variable "nic_name" {
-  description = "The name of the network interface for the Linux VM"
+variable "vm_admin_password" {
+  description = "Admin password for the VM"
   type        = string
 }
 
-# Virtual Machine Size (e.g., Standard_DS1_v2)
 variable "vm_size" {
-  description = "The size of the virtual machine"
+  description = "Size of the Virtual Machine"
   type        = string
 }
 
-# Admin username for the Linux VM
-variable "admin_username" {
-  description = "The admin username for the Linux VM"
-  type        = string
-}
-
-# Admin password for the Linux VM
-variable "admin_password" {
-  description = "The admin password for the Linux VM"
-  type        = string
-}
-
-# SSH Public Key for authentication
-variable "ssh_public_key_path" {
-  description = "The path to the SSH public key file"
-  type        = string
-}
-
-# Linux VM Image Details
 variable "vm_image_publisher" {
-  description = "The publisher of the Linux VM image"
+  description = "Publisher for the VM image"
   type        = string
 }
 
 variable "vm_image_offer" {
-  description = "The offer for the Linux VM image"
+  description = "Offer for the VM image"
   type        = string
 }
 
 variable "vm_image_sku" {
-  description = "The SKU for the Linux VM image"
+  description = "SKU for the VM image"
   type        = string
 }
 
 variable "vm_image_version" {
-  description = "The version of the Linux VM image"
-  type        = string
-}
-
-# Docker installation script details
-variable "docker_repo_url" {
-  description = "The Docker repository URL"
-  type        = string
-}
-
-variable "java_version" {
-  description = "The version of Java to install"
+  description = "Version for the VM image"
   type        = string
 }
